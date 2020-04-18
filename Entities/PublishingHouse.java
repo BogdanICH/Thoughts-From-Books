22a4cf106045100e947b63ad828816feb8d889a4
Iordache Bogdan-Mihai <48602122+BogdanICH@users.noreply.github.com>
2020-04-18 18:11:12 +0300

    Update PublishingHouse.java

--- Entities/PublishingHouse.java
+++ Entities/PublishingHouse.java
@@ -1,80 +1,32 @@
 package ro.unibuc.fmi;
 
+import javax.print.DocFlavor;
+import java.security.PrivateKey;
 import java.util.HashSet;
 import java.util.Set;
 
 public class PublishingHouse {
+    private String name;
 
-    private String Name;
+    Set<Author> authorList = new HashSet<>();
 
-    //lista de carti
-    Set<Book> booksSet = new HashSet<>();
-
-    //constructor
-    public PublishingHouse(String Name) {
-        this.Name = Name;
+    public PublishingHouse(String name) {
+        this.name = name;
     }
 
-    //adauga carte
-    public void addBook(String authorName, String nationality, String name, Integer numberOfPages, Integer rating) {
-        Book book = new Book(authorName, nationality, name, numberOfPages, rating);
-        booksSet.add(book);
-    }
-
-    //returneaza numarul de pagini a unei carti
-    public Integer getBookPages(String name) {
-        for (Book book : booksSet) {
-            if (book.getName().equals(name)) {
-                return book.getNumberOfPages();
-            }
+    public Integer getNumberOfBooks() {
+        int noOfBooks = 0;
+        for (Author author : authorList) {
+            noOfBooks += author.numberOfBooks();
         }
-        return 0;
-    }
-    //returneaza ratingul unei carti
-    public Integer getBookRating(String name) {
-        for (Book book : booksSet) {
-            if (book.getName().equals(name)) {
-                return book.getRating();
-            }
-        }
-        return 0;
+        return noOfBooks;
     }
 
-    //afiseaza cartile din editura asta
-    public void showBooks() {
-        for (Book book : booksSet) {
-            System.out.println(book.getName());
-        }
-    }
-
-    //afisez o singura carte
-    public void showBook(String bookName) {
-        for (Book book : booksSet) {
-            if (book.getName().equals(bookName)) {
-                System.out.println("Title: " + book.getName() + " | Author: " + book.getAuthor() + " | pages: " + book.getNumberOfPages() + " | Rating: " + book.getRating());
-                book.getNotes();
-            }
-        }
-    }
-
-    //adaugare nota
-    public Boolean addNote(String bookName, String note, Integer page) {
-        for (Book book : booksSet) {
-            if (book.getName().equals(bookName)) {
-                book.addNote(note, page);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    //numarul de carti din editura asta
-    public Integer noOfBooksInThis() {
-        return booksSet.size();
+    public void addAuthor(Author author) {
+        authorList.add(author);
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
-
 }

f4d652893f1a8563b41ddab5432ac504f635d401
Iordache Bogdan-Mihai <48602122+BogdanICH@users.noreply.github.com>
2020-04-18 18:12:57 +0300

    Create Human.java

---
+++ Entities/Human.java
@@ -0,0 +1,11 @@
+package ro.unibuc.fmi;
+
+public class Human {
+    protected String name;
+    protected String nationality;
+
+    public Human(String name, String nationality) {
+        this.name = name;
+        this.nationality = nationality;
+    }
+}
